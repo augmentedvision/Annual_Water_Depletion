4db124004081cffce590818422d4071b6f216c79
Htay Hlaing Win <85615744+augmentedvision@users.noreply.github.com>
2023-07-10 16:06:49 +1000

    Commit

--- water_depletion.py
+++ water_depletion.py
@@ -1,11 +1,24 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-df = pd.read_excel(r'/Users/brandonwu/Downloads/ANNUAL_WATER_DEPLETION.xls')
-print(df.shape)
-#print(df)
-new_header = df.iloc[2].tolist() #grab the first row for the header
-df = df[1:] #take the data less the header row
-df.columns = new_header #set the header row as the df header
-print(df.columns.tolist())
+import os
+from datetime import datetime
 
-df["Country Name"].value_counts().plot(kind="bar")
-print(df["2020.0"].value_counts())
+df = pd.concat(
+    pd.read_excel(
+        r"/Users/brandonwu/Downloads/ANNUAL_WATER_DEPLETION.xls", sheet_name=None
+    ),
+    ignore_index=True,
+)
+
+print(df.shape)
+
+new_header = df.iloc[2].tolist()  # grab the third row for the header
+df = df[1:]  # take the data less the header row
+df.columns = new_header  # set the header row as the df header
+print(df.columns.tolist())
+print(df.shape)
+# df["Country Name"].value_counts().plot(kind="bar")
+# print(df["2020"].value_counts())
+# df["2020"].plot().show()
