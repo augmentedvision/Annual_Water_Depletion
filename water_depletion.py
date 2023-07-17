dcd375cedea1ed828d12ac6d707088b230bb5037
Htay Hlaing Win <85615744+augmentedvision@users.noreply.github.com>
2023-07-17 15:06:11 +1000

    Commit

--- water_depletion.py
+++ water_depletion.py
@@ -5,20 +5,18 @@ import pandas as pd
 import os
 from datetime import datetime
 
-df = pd.concat(
-    pd.read_excel(
-        r"/Users/brandonwu/Downloads/ANNUAL_WATER_DEPLETION.xls", sheet_name=None
-    ),
-    ignore_index=True,
-)
+df1 =pd.read_excel(
+        r"/Users/brandonwu/Downloads/ANNUAL_WATER_DEPLETION.xls")
 
-print(df.shape)
+print(df1.shape)
 
-new_header = df.iloc[2].tolist()  # grab the third row for the header
-df = df[1:]  # take the data less the header row
-df.columns = new_header  # set the header row as the df header
-print(df.columns.tolist())
-print(df.shape)
+new_header = df1.iloc[2].tolist()  # grab the third row for the header
+df1 = df1[1:]  # take the data less the header row
+df1.columns = new_header  # set the header row as the df header
+print(df1.columns.tolist())
+
+df2 = pd.read_excel
+#print(df.shape)
 # df["Country Name"].value_counts().plot(kind="bar")
 # print(df["2020"].value_counts())
 # df["2020"].plot().show()
